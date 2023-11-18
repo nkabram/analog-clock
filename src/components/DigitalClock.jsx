af52504754ec91e6e0219b7f789889b86b95db5e
Nick Abram <nkabram@gmail.com>
2023-11-18 12:41:51 -0800

    Updated digital clock, no year

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -3,27 +3,35 @@ import { useState, useEffect } from 'react';
 function DigitalClock() {
     const [date, setDate] = useState(new Date().getDate());
     const [month, setMonth] = useState(new Date().getMonth() + 1); // Months are 0-based in JavaScript
-    const [year, setYear] = useState(new Date().getFullYear());
     const [hours, setHours] = useState(new Date().getHours());
     const [minutes, setMinutes] = useState(new Date().getMinutes());
-    const [seconds, setSeconds] = useState(new Date().getSeconds());
-
+    const [ampm, setAmPm] = useState(new Date().getHours() < 12 ? 'AM' : 'PM');
+    
     useEffect(() => {
         const interval = setInterval(() => {
             const now = new Date();
             setDate(now.getDate());
             setMonth(now.getMonth() + 1);
-            setYear(now.getFullYear());
             setHours(now.getHours());
             setMinutes(now.getMinutes());
-            setSeconds(now.getSeconds());
+
         }, 1000);
         return () => clearInterval(interval);
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+          const now = new Date();
+          // ...
+          setHours(now.getHours() % 12 || 12); // Convert to 12-hour format
+          setAmPm(now.getHours() < 12 ? 'AM' : 'PM');
+        }, 1000);
+        return () => clearInterval(interval);
+      }, []);
+
     return (
         <div className="digital-clock">
-            {month}/{date} - {hours}:{minutes}:{seconds}
+            {month}/{date} {hours}:{minutes} {ampm}
         </div>
     );
 }
